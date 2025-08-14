83d28703a623452f79677fa142d0b5aa6d377e73
DerrickTheRanger <38844745+DerrickTheRanger@users.noreply.github.com>
2025-08-14 14:37:25 -0400

    Create jellyfin.sh

---
+++ jellyfin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker run -d \
+ --name jellyfin \
+ --user uid:gid \
+ --net=host \
+ --volume /jellyfin/config:/config \
+ --volume /jellyfin/cache:/cache \
+ --mount type=bind,source=/jellyfin/media,target=/media \
+ --restart=unless-stopped \
+jellyfin/jellyfin
